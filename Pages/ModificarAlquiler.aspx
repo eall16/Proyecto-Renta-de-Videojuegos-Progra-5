57f1cb72fe5f9459e178d271722d2137f2db7c48
allanjvr0812 <allanjvr08@gmail.com>
2026-07-30 18:09:28 -0600

    Se crea y se sube Modificar Alquiler

---
+++ Pages/ModificarAlquiler.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Title="Modificar alquiler" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ModificarAlquiler.aspx.cs" Inherits="RentaVideojuegos.Pages.ModificarAlquiler" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1>Modificar alquiler</h1>
+
+    <asp:Label ID="lblMensaje" runat="server" Visible="false"></asp:Label>
+
+    <table class="tabla-detalle">
+        <tr>
+            <td># Alquiler:</td>
+            <td><asp:Label ID="lblIdAlquiler" runat="server" /></td>
+        </tr>
+
+        <tr>
+            <td>Jugador:</td>
+            <td><asp:Label ID="lblJugador" runat="server" /></td>
+        </tr>
+
+        <tr>
+            <td>Sucursal:</td>
+            <td><asp:Label ID="lblSucursal" runat="server" /></td>
+        </tr>
+
+        <tr>
+            <td>Videojuego:</td>
+            <td><asp:Label ID="lblVideojuego" runat="server" /></td>
+        </tr>
+    </table>
+
+    <table class="tabla-formulario">
+        <tr>
+            <td>Fecha de inicio:</td>
+            <td>
+                <asp:TextBox ID="txtFechaInicio" runat="server" CssClass="form-control" TextMode="Date"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvFechaInicio" runat="server"
+                    ControlToValidate="txtFechaInicio"
+                    ErrorMessage="La fecha de inicio es requerida."
+                    ForeColor="Red">
+                </asp:RequiredFieldValidator>
+            </td>
+        </tr>
+
+        <tr>
+            <td>Fecha de devolución:</td>
+            <td>
+                <asp:TextBox ID="txtFechaDevolucion" runat="server" CssClass="form-control" TextMode="Date"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvFechaDevolucion" runat="server"
+                    ControlToValidate="txtFechaDevolucion"
+                    ErrorMessage="La fecha de devolución es requerida."
+                    ForeColor="Red">
+                </asp:RequiredFieldValidator>
+            </td>
+        </tr>
+    </table>
+
+    <p>
+        <asp:Button ID="btnGuardar" runat="server"
+            Text="Guardar cambios"
+            CssClass="btn btn-primary"
+            OnClick="btnGuardar_Click" />
+
+        <asp:Button ID="btnRegresar" runat="server"
+            Text="Regresar"
+            CssClass="btn btn-secondary"
+            CausesValidation="false"
+            OnClick="btnRegresar_Click" />
+    </p>
+
+</asp:Content>
---
+++ Pages/ModificarAlquiler.aspx.designer.cs
@@ -0,0 +1,116 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace RentaVideojuegos.Pages
+{
+
+
+    public partial class ModificarAlquiler
+    {
+
+        /// <summary>
+        /// Control lblMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensaje;
+
+        /// <summary>
+        /// Control lblIdAlquiler.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblIdAlquiler;
+
+        /// <summary>
+        /// Control lblJugador.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblJugador;
+
+        /// <summary>
+        /// Control lblSucursal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSucursal;
+
+        /// <summary>
+        /// Control lblVideojuego.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblVideojuego;
+
+        /// <summary>
+        /// Control txtFechaInicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFechaInicio;
+
+        /// <summary>
+        /// Control rfvFechaInicio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvFechaInicio;
+
+        /// <summary>
+        /// Control txtFechaDevolucion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFechaDevolucion;
+
+        /// <summary>
+        /// Control rfvFechaDevolucion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvFechaDevolucion;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+
+        /// <summary>
+        /// Control btnRegresar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegresar;
+    }
+}
